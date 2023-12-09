7f87514633be886be34bb9cc38f6157c47575ce1
PiyushG-dev <pjgpiyush@gmail.com>
2023-12-09 23:19:53 +0530

    array insert finished

--- array_insert/script.js
+++ array_insert/script.js
@@ -1,1 +1,24 @@
-console.log("loaded");
+const data = [20, 50, 90, 30, 100, 170, 190, 10];
+
+const btn = document.getElementById("btn");
+console.log(data);
+
+btn.addEventListener("click", (e) => {
+  let element = document.getElementById("element").value;
+  let position = document.getElementById("position").value;
+  e.preventDefault();
+
+  if (!isNaN(element) && !isNaN(position)) {
+    for (let i = data.length - 1; i >= position; i--) {
+      data[i + 1] = data[i];
+
+      if (i == position) {
+        data[i] = parseInt(element);
+      }
+    }
+  } else {
+    alert("enter valid value");
+  }
+
+  console.log(data);
+});
