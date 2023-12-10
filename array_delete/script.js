7eed83581160c32271d11fd3bfd3540521ed3656
PiyushG-dev <pjgpiyush@gmail.com>
2023-12-10 18:57:47 +0530

    array delete complete

--- array_delete/script.js
+++ array_delete/script.js
@@ -1,1 +1,18 @@
-console.log("loaded");
+let data = [10, 25, 46, 78, 12, 16, 55, 34];
+let btn = document.getElementById("btn");
+console.log(data);
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let position = document.getElementById("position").value;
+
+  if (!isNaN(position)) {
+    for (let i = parseInt(position); i < data.length - 1; i++) {
+      data[i] = data[i + 1];
+    }
+    data.length = data.length - 1;
+    console.log(data);
+  } else {
+    alert("enter valid number");
+  }
+});
