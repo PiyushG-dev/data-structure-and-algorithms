7caa19e4c83e921b81d28c0c8c9745c5324edb4e
PiyushG-dev <pjgpiyush@gmail.com>
2023-12-10 20:52:24 +0530

    array search finished

--- array_search/script.js
+++ array_search/script.js
@@ -1,1 +1,23 @@
-console.log("loaded");
+let data = [12, 16, 29, 57, 48, 14, 33];
+let index = undefined;
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let item = document.getElementById("item").value;
+
+  if (!isNaN(item)) {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i] === parseInt(item)) {
+        index = i;
+        break;
+      }
+    }
+  } else {
+    alert("enter valid number");
+  }
+
+  console.log(index);
+});
